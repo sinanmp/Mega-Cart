0d400621812fb528229f2093911cd75c8ec138dc
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-03-11 20:03:31 +0530

    api addedd

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// const addressSchema = new Schema({
-//     street: String,
-//     city: String,
-//     state: String,
-//     pinCode: String,
-//     country: String,
-//   });
 
 var schema = new mongoose.Schema({
     username: {
