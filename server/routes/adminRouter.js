432895a676a047f63ffa1ab94e6006f329e06b8e
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-03-11 20:09:39 +0530

    api again updated

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -1,108 +1,90 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controller/userController");
-const userServices = require('../services/userRender')
-const cartController = require("../controller/cartController");
-const wishlistController=require("../controller/wishlistController")
-const orderController=require("../controller/orderController")
-const {paymentMiddleware ,isAuthMiddleware} = require("../routes/middlewares/userMiddleware");
-const reviewController= require("../controller/reviewController")
+const adminController = require("../controller/adminController");
+const adminServices = require('../services/adminRender');
+const productController = require("../controller/productController");
+const categoryController = require("../controller/categoryController")
+const adminAuthMiddleware = require("../routes/middlewares/adminMiddleware")
+const orderController = require("../controller/orderController")
+const coupenController = require("../controller/coupenController")
+const bannerController= require("../controller/bannerController")
+const upload = require("../routes/middlewares/imageUpload")
 
 
-router.get('/', userController.userHome)//home
-router.get('/login', userServices.login)
-router.get('/signup', userServices.signup);
-router.get("/userDetails",isAuthMiddleware, userServices.userDeatails)
+//login
+router.get("/adminLogin", adminAuthMiddleware.adminIsnotAuth, adminServices.login);
+
+router.post("/api/admin1", adminController.register);
 
 
-router.get("/take/productPrice",userController.takeProductPrice)
-router.get("/single/prd", userController.singlePrd)
-router.get("/cart",isAuthMiddleware, cartController.cart)
-router.get("/cartTo/wishlist",isAuthMiddleware,wishlistController.cartToWishlist)
-router.get("/block-route",userServices.block)
+
+//admin contents     
+router.get("/adminDash", adminAuthMiddleware.adminIsAuth, adminServices.dash)
+router.get("/admin-order", adminAuthMiddleware.adminIsAuth, adminServices.order)
+router.get("/order/list/admin", adminController.orderList)
+router.get("/admin-users", adminAuthMiddleware.adminIsAuth, adminServices.users)
+router.get("/add-products", adminAuthMiddleware.adminIsAuth, productController.addingProduct)
+router.get("/api/users", adminController.find);
+router.post("/get/chart",adminController.getChart)
 
 
-router.get("/count/update-quantity",isAuthMiddleware, cartController.updateQuantity);
-router.get("/count/get-total-price", cartController.getTotalPrice);
-router.get("/remove/cart",isAuthMiddleware, cartController.removeCart)
-router.get("/cart/api",isAuthMiddleware, cartController.addtocart)
-router.get("/resend/otp", userController.resendOtp)      
-router.get("/user/verify", userController.userverify)   
-router.post("/verify/api", userController.verify)
-router.get("/api/logout", userController.logout)
-router.post("/api/create", userController.create);//use register api
-router.post('/api/login', userController.find)//user login api   
-router.get("/address/api",isAuthMiddleware, userServices.address)    
-router.post("/address/add",isAuthMiddleware, userController.addAddress)
-router.get("/update/address",isAuthMiddleware, userController.updateAddress)
-router.post("/update/add",isAuthMiddleware, userController.updatePostAdd)
-//our store
-router.get("/our-store", userController.ourStore)
-router.get("/changeIndex/api", userController.changeIndex)
-router.get("/address/delete",isAuthMiddleware, userController.adDelete)
-router.get("/update/passCheck",isAuthMiddleware, userController.updateCheck)
-router.post("/Update/userRender", userController.updateUser)
-router.post("/update/post/user", userController.updatePost)
-//module export
 
-
-//forgot pass
-router.post("/address/addPostCheckout",isAuthMiddleware,userController.addressPostCheckout)
-router.post("/checkout/address",isAuthMiddleware,userServices.changeAddress)
-router.post("/chekcout/addAddress",isAuthMiddleware,userServices.checkoutAddAddress)
-router.get("/api/checkout",userController.checkoutFetch)     
-router.get("/checkout",userServices.checkout) 
-router.get("/forgot/get", userServices.forgotget)
-router.post("/send/otp/forgot", userController.sendOtpForgot)
-router.post("/forgot/Verify", userController.forgotVerify)  
-router.post("/main/forgotPass", userController.mainforgetPass)
-router.get("/forgot/Verify", userServices.redirectForgot)
-router.post("/setSession",userServices.setSession)
-
-
-      
-//wishlists api
-router.get('/wish/api/cart',wishlistController.wishaddcart)
-router.get("/wish/api",wishlistController.wishAdd)
-router.get("/wishlist",wishlistController.goTowishlist)
-router.get("/remove/wishlist",wishlistController.removeWishlist)
-router.get("/wish/api/home",wishlistController.wishApiHome)
-router.get("/wishlist/addtoCart",cartController.wishlistToCart)
-
-
+//product managment
+router.get("/admin-products", adminAuthMiddleware.adminIsAuth, productController.products)
+router.post("/api/add-product", upload, productController.addProduct);
+router.get("/updateProduct", productController.updateProcduct)
+router.post("/api/update/product", productController.productUpdate)
+router.get("/api/delete/product", productController.deleteProduct)
+router.get("/unlist/api", productController.unlist)
+router.get("/remove/unlist", productController.removeUnlist)
+router.get("/api/block", adminController.block);
+router.get("/user-details", adminServices.usersDetails);
 
 //order managment
-router.post("/submit/order",orderController.submitOrder)
-router.get("/order/success",userServices.orderSuccess)
-router.get("/user/orders",userServices.userOrders)
-router.get("/order/list/user",userController.orderList)
-router.get ("/cancel/order",orderController.cancel)
-router.get("/api/reason",userServices.reason)
-router.post("/cancel/reason",orderController.cancelMain)
-router.get("/user-order/details",userServices.userOrdersDeatails)
-router.post("/payment",paymentMiddleware,userServices.payment)
-router.post("/order-route",orderController.orderRoute)
-router.get("/cart/count",cartController.cartCountPr)
-router.get("/fetch-order",orderController.fetchOrderD)
-router.get("/cancel/singleProduct",orderController.singleCancel)
-router.get("/return/order",orderController.returnOrder)
-
-//wallet
-router.get("/wallet",isAuthMiddleware,userServices.wallet)
-router.post("/wallet-add",userController.walletAdd)
-router.get("/wallet-fetch",userController.walletFetch)
-router.get("/wallet/dataUpdate",userController.walletUpdate)
-router.get("/fetch-wallet-total",userController.fetchTotalWalletAmount)
-router.post("/wallet-payment",userController.walletPayment)
-  
-//
-router.post("/applyCoupon",userController.applyCoupen)
-router.get("/coupon-cancel",userController.couponCancel)
+router.post("/change/status", orderController.changeStatus)
+router.get("/admin/individualOrder",orderController.individualOrder)
 
 
 
-//reviews
-router.get("/add/review",reviewController.reviewGet)
-router.post("/review-submit",reviewController.reviewSubmit)
+//image 
+router.get("/image-delete", productController.removeImage)
+router.post("/upload/img", upload, productController.uploadImage);
+router.get("/add/images", productController.addImages)
+
+router.get("/api/countUsers", adminController.countUsers)
+router.get("/api/countOrders", adminController.countOrders)
+
+
+//category managment
+router.get("/category/api", categoryController.category)
+router.get("/single/cat", categoryController.singleCat)
+router.get("/add/category", categoryController.addCategory)
+router.post("/add/cat/post", categoryController.addCatPost)
+router.get("/delete/cat", categoryController.deleteCat)
+router.get("/unlisted/cat", categoryController.unlist)
+router.get("/remove/funlist", categoryController.removeFUnlist)
+router.get("/admin/logout", adminServices.logout)
+
+
+//coupens managment
+router.get("/coupen/api", adminServices.GetCoupenPage)
+router.get("/addCoupen/api", adminServices.addCoupen)
+router.post("/addCoupen/post", coupenController.addCoupenPost)
+router.get("/getCoupens", coupenController.getCoupens)
+router.get("/expiredCoupens", coupenController.expiredCoupens)
+router.get("/unlist/coupens", coupenController.unlistCoupens)
+router.get("/restore/coupens", coupenController.restoreCoupens)
+
+
+ 
+
+//banner managment
+router.get("/banners/api",bannerController.getBanners)
+router.get("/addBanner",bannerController.addBanner)
+router.post("/addCoupen",upload ,bannerController.addPost)
+router.get("/unlist-banner",bannerController.unsilst)
+router.get("/Unlisted-Banners",bannerController.unlistedBanners)
+router.get("/restore-banner",bannerController.restoreBanner)
+
 
 module.exports = router;
