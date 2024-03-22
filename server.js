e32373558643114653f190edd78f4f72896f87e9
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-03-22 22:08:45 +0530

    new Featadfsar

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(session({
 }));
 
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
 // app.use((req, res, next) => {
 //   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 //   res.setHeader("Pragma", "no-cache");
