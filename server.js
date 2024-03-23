55442051194e67bc0e0ed1b3d4f5a83273f8b07e
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-03-23 10:45:38 +0530

    response

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-
+ 
 
 // app.use((req, res, next) => {
 //   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
--- server/controller/tudoController.js
+++ server/controller/tudoController.js
@@ -3,25 +3,58 @@ const tudoDb = require('../model/tudoSchema')
 
 
 exports.getData =async (req,res)=>{
-    const data = await tudoDb.find()
-    res.json(data)
+    try {
+        const data = await tudoDb.find()
+        res.json(data)   
+    } catch (error) {
+        res.json(error,'from try catch')
+
+    }
+
 }
 
 
 exports.addData= async (req,res)=>{
-    const {text} = req.body
-    const obj = new tudoDb({
-        text : text
-    })
-    await obj.save()
+    try {
+        const {text} = req.body
+        console.log(req.body,'this is text')
+        const obj = new tudoDb({
+            text : text
+        })
+        const data = await tudoDb.find({text:text})
+        if(data.length){
+            console.log(data)
+            return res.json("this task is already added")
+        }else{
+            await obj.save()
+            res.json("data added successfully")
+        }
+    } catch (error) {
+        res.json(error,'from try catch') 
+    }
+
 }
 
 
 exports.checked= async(req,res)=>{
-    await tudoDb.updateOne({_id:req.query.id},{$set:{checked:true}})
+    try {
+        await tudoDb.updateOne({_id:req.query.id},{$set:{checked:true}})
+        const data = await tudoDb.find()
+        res.json(data)
+    } catch (error) {
+        res.json({error:error})
+    }
+  
 }
 
-
+ 
 exports.delete=async(req,res)=>{
-    await tudoDb.deleteOne({_id:req.query.id})
-}
+    try {
+        await tudoDb.deleteOne({_id:req.query.id})
+        const data = await tudoDb.find()
+        res.json(data)
+    } catch (error) {
+        res.json({error:error})
+    }
+ 
+} 
